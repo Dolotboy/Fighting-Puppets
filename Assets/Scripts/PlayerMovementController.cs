e89ed70cf24e7947a86ae9e5b4ffdba01ff887b5
NoahPM <noahplourdem@hotmail.com>
2022-11-04 16:00:06 -0400

    2 Players

---
+++ Assets/Scripts/PlayerMovementController.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+using UnityEngine.SceneManagement;
+using Random = System.Random;
+
+public class PlayerMovementController : NetworkBehaviour
+{
+    public float Speed = 0.1f;
+    public GameObject PlayerModel;
+
+    private void Start()
+    {
+        PlayerModel.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (SceneManager.GetActiveScene().name == "Game")
+        {
+            if (PlayerModel.activeSelf == false)
+            {
+                SetPosition();
+                PlayerModel.SetActive(true);
+            }
+
+            if (hasAuthority)
+            {
+                Movement();
+            }
+        }
+    }
+
+    public void SetPosition()
+    {
+        transform.position = new Vector3(0, 1, 0);
+    }
+
+    public void Movement()
+    {
+        float xDirection = Input.GetAxis("Horizontal");
+        float zDirection = Input.GetAxis("Vertical");
+
+        Vector3 moveDirection = new Vector3(xDirection, 0.0f, zDirection);
+
+        transform.position += moveDirection * Speed;
+    }
+}
