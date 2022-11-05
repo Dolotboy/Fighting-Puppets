81049ce1d5e68debcf045912d287d42b026f7159
MaximeLepageUQAC <113078725+MaximeLepageUQAC@users.noreply.github.com>
2022-11-05 16:40:33 -0400

    +Map selector loader

--- Assets/Scripts/ScrollViewMapLoader.cs
+++ Assets/Scripts/ScrollViewMapLoader.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
@@ -26,24 +27,36 @@ public class ScrollViewMapLoader : MonoBehaviour
 
     public List<Map> versusMaps = new List<Map>();
 
+    public Dropdown gamemodeDropdown;
+
     [SerializeField] private Transform scrollViewContent;
 
     [SerializeField] private GameObject prefab;
 
     void Start()
     {
+
+        //gamemodeDropdown = GetComponent<Dropdown>();
+        //Add listener for when the value of the Dropdown changes, to take action
+        gamemodeDropdown.onValueChanged.AddListener(delegate {
+            DropdownValueChanged(gamemodeDropdown);
+        });
+
         coopMaps.Add(new Map("Arena", "CoopArena", Resources.Load<Sprite>("MapImages/Arena"), 1));
         coopMaps.Add(new Map("Mystic Forest", "MysticForest", Resources.Load<Sprite>("MapImages/MysticForest"), 1));
 
-        loadCoopMaps();
-
 
         versusMaps.Add(new Map("Arena", "VersusArena", Resources.Load<Sprite>("MapImages/Arena"), 3));
+        versusMaps.Add(new Map("SpaceShip", "VersusSpaceShip", Resources.Load<Sprite>("MapImages/SpaceShip"), 3));
+
+        loadCoopMaps();
     }
 
     void loadCoopMaps()
     {
-        foreach(Map map in coopMaps)
+        destroyChild();
+
+        foreach (Map map in coopMaps)
         {
             GameObject newMap = Instantiate(prefab, scrollViewContent);
             if(newMap.TryGetComponent<MapScrollViewItem>(out MapScrollViewItem item))
@@ -56,6 +69,36 @@ public class ScrollViewMapLoader : MonoBehaviour
 
     void loadVersusMaps()
     {
+        destroyChild();
 
+        foreach (Map map in versusMaps)
+        {
+            GameObject newMap = Instantiate(prefab, scrollViewContent);
+            if (newMap.TryGetComponent<MapScrollViewItem>(out MapScrollViewItem item))
+            {
+                item.ChangeImage(map.sprite);
+                item.ChangeText(map.name);
+            }
+        }
+    }
+
+    void DropdownValueChanged(Dropdown change)
+    {
+        if(change.value == 0) // COOP
+        {
+            loadCoopMaps();
+        }
+        else
+        {
+            loadVersusMaps();
+        }
+    }
+
+    private void destroyChild()
+    {
+        foreach (Transform child in transform) // For each child in current gameObject
+        {
+            GameObject.Destroy(child.gameObject); // Destroy the child
+        }
     }
 }
