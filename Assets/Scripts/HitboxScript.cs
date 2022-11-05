fac2f78dcbf1bb12bb4ba4180677ffd850b60280
NoahPM <noahplourdem@hotmail.com>
2022-11-05 16:07:45 -0400

    added authorities

--- Assets/Scripts/HitboxScript.cs
+++ Assets/Scripts/HitboxScript.cs
@@ -1,13 +1,16 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Mirror;
 using UnityEngine;
 
-public class HitboxScript : MonoBehaviour
+public class HitboxScript : NetworkBehaviour
 {
     [SerializeField] GameObject PlayerObject;
     private void OnTriggerEnter(Collider collider)
     {
+        if(!hasAuthority){ return;}
+        
         if (collider.gameObject.CompareTag("Weapon"))
         {
             PlayerObject.GetComponent<Health>().TakeHit(gameObject.tag);
