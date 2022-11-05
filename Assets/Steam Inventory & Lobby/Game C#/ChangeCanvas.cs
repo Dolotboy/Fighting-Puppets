b66df564c7598e39c46cc3a46b71b8578d92c495
MaximeLepageUQAC <113078725+MaximeLepageUQAC@users.noreply.github.com>
2022-11-05 17:34:59 -0400

    +Host map selector menu

--- Assets/Steam Inventory & Lobby/Game C#/ChangeCanvas.cs
+++ Assets/Steam Inventory & Lobby/Game C#/ChangeCanvas.cs
@@ -8,17 +8,39 @@ namespace BrettArnett
     {
         public GameObject CanvasA;
         public GameObject CanvasB;
+        public GameObject CanvasC;
+        public GameObject CanvasD;
 
         public void ToA()
         {
-            CanvasA.SetActive(true);
             CanvasB.SetActive(false);
+            CanvasC.SetActive(false);
+            CanvasD.SetActive(false);
+            CanvasA.SetActive(true);
         }
 
         public void ToB()
         {
             CanvasA.SetActive(false);
+            CanvasC.SetActive(false);
+            CanvasD.SetActive(false);
             CanvasB.SetActive(true);
+        }
+
+        public void ToC()
+        {
+            CanvasA.SetActive(false);
+            CanvasB.SetActive(false);
+            CanvasD.SetActive(false);
+            CanvasC.SetActive(true);
+        }
+
+        public void ToD()
+        {
+            CanvasA.SetActive(false);
+            CanvasB.SetActive(false);
+            CanvasC.SetActive(false);
+            CanvasD.SetActive(true);
         }
     }
 }
