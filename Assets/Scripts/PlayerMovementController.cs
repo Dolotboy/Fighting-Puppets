ca847eaf53fc7a8cf47b873ca33aaf419d2eeaff
NoahPM <noahplourdem@hotmail.com>
2022-11-07 12:55:19 -0500

    Fix??

--- Assets/Scripts/PlayerMovementController.cs
+++ Assets/Scripts/PlayerMovementController.cs
@@ -80,8 +80,8 @@ public class PlayerMovementController : NetworkBehaviour
             if (hasAuthority)
             {
                 Movement();
+                Interact();
             }
-            Interact();
         }
     }
 
