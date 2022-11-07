e2c8e3da48a3b62c2bd9dbf97f0a21c3d9deb9e0
NoahPM <noahplourdem@hotmail.com>
2022-11-06 21:55:20 -0500

    Fixed Weapons and added plane to lobby

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -24,23 +24,6 @@ public class InputManager : NetworkBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(!hasAuthority) { return; }
-        if(Input.GetKeyDown(escapeMenuInput) && SceneManager.GetActiveScene().name != "Scene_Lobby" && SceneManager.GetActiveScene().name != "Scene_Steamworks")
-        {
-            Cursor.visible = !Cursor.visible;
-
-            if (escapeMenu.activeInHierarchy)
-            {
-                Cursor.lockState = CursorLockMode.Locked;
-                escapeMenu.SetActive(false);
-            }
-            else
-            {
-                Cursor.lockState = CursorLockMode.None;
-                escapeMenu.SetActive(true);
-            }
-        }
-
         RaycastHit hit;
         var camTrasform = cam.transform;
         Ray ray = new Ray(camTrasform.position, camTrasform.forward);
@@ -58,5 +41,23 @@ public class InputManager : NetworkBehaviour
                 Destroy(hit.transform.gameObject);
             }
         }
+        if(!hasAuthority) { return; }
+        if(Input.GetKeyDown(escapeMenuInput) && SceneManager.GetActiveScene().name != "Scene_Lobby" && SceneManager.GetActiveScene().name != "Scene_Steamworks")
+        {
+            Cursor.visible = !Cursor.visible;
+
+            if (escapeMenu.activeInHierarchy)
+            {
+                Cursor.lockState = CursorLockMode.Locked;
+                escapeMenu.SetActive(false);
+            }
+            else
+            {
+                Cursor.lockState = CursorLockMode.None;
+                escapeMenu.SetActive(true);
+            }
+        }
+
+       
     }
 }
