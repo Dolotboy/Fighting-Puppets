1c9f4a2579d28eab95d99778ee0b6dac8df203b4
NoahPM <noahplourdem@hotmail.com>
2022-11-09 23:06:43 -0500

    potential fix

--- Assets/Scripts/WeaponScript.cs
+++ Assets/Scripts/WeaponScript.cs
@@ -13,8 +13,47 @@ public class WeaponScript : MonoBehaviour
         return damageModifier;
     }
 
-    public void GetDamageDone()
+    public void GetDamageDone(string tag)
     {
-        Debug.Log("Damage Done to Enemy");
+        switch (tag)
+        {
+            case "Hitbox_Head":
+            {
+                Debug.Log($"Hit done to Head {10 * damageModifier}");
+            } break;
+            case "Hitbox_Torso":
+            {
+                Debug.Log($"Hit done to Torso {7 * damageModifier}"); 
+
+            } break;
+            case "Hitbox_Hips":
+            {
+                Debug.Log($"Hit done to Hips {4 * damageModifier}");
+            } break;
+            case "Hitbox_LeftLeg":
+            {
+                Debug.Log($"Hit done to Left Leg {2 * damageModifier}");
+            } break;
+            case "Hitbox_RightLeg":
+            {
+                Debug.Log($"Hit done to Right Leg {2 * damageModifier}");
+            } break;
+            case "Hitbox_LeftArm":
+            {
+                Debug.Log($"Hit done to Left Arm {1 * damageModifier}");
+            } break;
+            case "Hitbox_RightArm":
+            {
+                Debug.Log($"Hit done to Right Arm {1 * damageModifier}");
+            } break;
+            case "Hitbox_LeftForeArm":
+            {
+                Debug.Log($"Hit done to Left Forearm {1 * damageModifier}");
+            } break;
+            case "Hitbox_RightForeArm":
+            {
+                Debug.Log($"Hit done to Right Forearm {1 * damageModifier}");
+            } break;
+        }
     }
 }
